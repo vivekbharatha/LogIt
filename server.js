c56e23bd875e703d0fd950ff4aaae886cd19157a
Vivek Bharatha <vivek.bharatha@gmail.com>
2016-01-17 15:41:20 +0530

    fixed static files issue

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = express();
 
 var port = process.env.PORT || 3000;
 
-server.use(express.static(path.join(__dirname + '/ngApp')));
+server.use("/", express.static(path.join(__dirname + '/ngApp')));
 server.use(morgan('dev'));
 server.listen(port, function() {
     console.log('Server running at port: ' + port);
